2f69687fee994e42f265727a35e92ab7af14510f
arnuvtandon <43461672+arnuvtandon@users.noreply.github.com>
2023-06-02 02:23:11 -0700

    [test when ds available] now takes 49 frames

--- rovr/video_ds.py
+++ rovr/video_ds.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import torch
 from torchvision.transforms import functional as F
+from tqdm import tqdm
 
 class VideoDataset(torch.utils.data.Dataset):
     def __init__(self, folder, transform=None):
@@ -11,14 +12,12 @@ class VideoDataset(torch.utils.data.Dataset):
         self.videos = os.listdir(folder)
     
     def corrupt_frame(self, frame):
-        # Create a random mask
+        # regular random mask logic
         mask = np.ones_like(frame)
         h, w, _ = frame.shape
         mask_x = np.random.randint(0, w-50)
         mask_y = np.random.randint(0, h-50)
         mask[mask_y:mask_y+50, mask_x:mask_x+50, :] = 0
-        
-        # Apply mask to frame
         corrupted_frame = frame * mask
         return corrupted_frame, mask
     
@@ -26,40 +25,53 @@ class VideoDataset(torch.utils.data.Dataset):
         return len(self.videos)
     
     def normalize(self, frame):
-        #Normalize between -1 and 1
-        return (frame / 0.5) - 1
+        # normalize it in -1 to 1 range
+        return (frame / 127.5) - 1
     
     def __getitem__(self, idx):
         video_path = os.path.join(self.folder, self.videos[idx])
         vidcap = cv2.VideoCapture(video_path)
         
-        fps = vidcap.get(cv2.CAP_PROP_FPS)
-        sampling_rate = int(np.ceil(fps))
-        
         frames = []
         corrupted_frames = []
         masks = []
         i = 0
+        
         while True:
             success, frame = vidcap.read()
             if not success:
                 break
+            
+            # resize and crop to get 512x512
+            h, w, _ = frame.shape
+            new_size = min(h, w)
+            startx = w//2 - new_size//2
+            starty = h//2 - new_size//2
+            frame = frame[starty:starty+new_size, startx:startx+new_size]
+            frame = cv2.resize(frame, (512, 512))
+            
+            # corrupt the frame
+            corrupted_frame, mask = self.corrupt_frame(frame)
+            
+            if self.transform is not None:
+                frame = self.transform(frame)
+                corrupted_frame = self.transform(corrupted_frame)
+                mask = self.transform(mask)
+            
+            frames.append(self.normalize(frame))
+            corrupted_frames.append(self.normalize(corrupted_frame))
+            masks.append(mask)
 
-            if i % sampling_rate == 0:
-                # Resize frame
-                frame = cv2.resize(frame, (480, 270))
-                
-                # Corrupt frame
-                corrupted_frame, mask = self.corrupt_frame(frame)
-                
-                if self.transform is not None:
-                    frame = self.transform(frame)
-                    corrupted_frame = self.transform(corrupted_frame)
-                    mask = self.transform(mask)
-                
-                frames.append(self.normalize(frame))
-                corrupted_frames.append(self.normalize(corrupted_frame))
-                masks.append(mask)
             i += 1
+            if i == 49:
+                break
+                
+        if i < 49:
+            raise Exception(f"video {self.videos[idx]} has less than 49 frames!")
         
         return torch.stack(corrupted_frames), torch.stack(frames), torch.stack(masks)
+
+    def load(self):
+        print('loading videos... hang tight!')
+        for i in tqdm(range(len(self.videos))):
+            self.__getitem__(i)
