1b6e6ccae8da846b4089a608f0988146dd10a28d
arnuvtandon <43461672+arnuvtandon@users.noreply.github.com>
2023-06-03 18:47:27 -0700

    stress tested lstm

--- rovr/action_lstm.py
+++ rovr/action_lstm.py
@@ -1,31 +1,31 @@
-class CustomLSTM(nn.Module):
-    def __init__(self, hidden_dim, num_layers, device):
-        super(SimpleLSTM, self).__init__()
+import torch
+import torch.nn as nn
+class ActionLSTM(nn.Module):
+    def __init__(self, hidden_dim, num_layers, batch_size, device):
+        super(ActionLSTM, self).__init__()
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
+        self.batch_size = batch_size
         self.device = device
 
         self.lstm = nn.LSTMCell(3, hidden_dim).to(device)
-        self.fc = nn.Linear(hidden_dim, 1).to(device)
-        
-    def forward(self, frames):
-        batch_size = frames.size(0)
-        hx = torch.zeros(batch_size, self.hidden_dim).to(self.device)
-        cx = torch.zeros(batch_size, self.hidden_dim).to(self.device)
+        self.fc = nn.Linear(hidden_dim, 3072).to(device) 
 
-        # Normalizing frame values
-        frames = frames.float() / 48
+        self.hx = torch.zeros(batch_size, self.hidden_dim).to(self.device)
+        self.cx = torch.zeros(batch_size, self.hidden_dim).to(self.device)
 
-        outputs = []
-        for i in range(frames.size(1)):  
-            frame_i = frames[:, i]
-            
-            hx, cx = self.lstm(frame_i, (hx, cx))
-            
-            out = self.fc(hx)
-            
-            outputs.append(out.unsqueeze(1))
+    def forward(self, action):
+        action = action.float() / 48
         
-        outputs = torch.cat(outputs, dim=1)
-        
-        return outputs
+        print(action.shape)
+
+        self.hx, self.cx = self.lstm(action, (self.hx, self.cx))
+
+        out = self.fc(self.hx)
+        out = out.view(-1, 32, 32, 3)  
+            
+        return out
+
+    def reset_hidden_states(self):
+        self.hx = torch.zeros(self.batch_size, self.hidden_dim).to(self.device)
+        self.cx = torch.zeros(self.batch_size, self.hidden_dim).to(self.device)
