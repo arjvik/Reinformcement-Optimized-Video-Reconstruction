06f3903b862e17feebe9f6db25ea969bf2805d6c
Aaditya-Prasad <50385321+Aaditya-Prasad@users.noreply.github.com>
2023-06-04 17:48:21 -0700

    rovr holds all other models and runs training + inference

---
+++ rovr/rovr.py
@@ -0,0 +1,138 @@
+import torch
+import lpips
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ROVR(nn.Module):
+    def __init__(self, actor1, critic1, actor2, critic2, video_encoder, history_encoder, local_net, vid_length, time_steps):
+        self.actor1 = actor1
+        self.critic1 = critic1
+        self.actor2 = actor2
+        self.critic2 = critic2
+        self.local_net = local_net
+
+        self.vid_length = vid_length
+        self.time_steps = time_steps
+
+        self.lpips = lpips.LPIPS(net='alex')
+        self.video_encoder = video_encoder
+        self.history_encoder = history_encoder
+
+        self.actor1_optimizer = torch.optim.Adam(self.actor1.parameters(), lr=1e-4)
+        self.critic1_optimizer = torch.optim.Adam(self.critic1.parameters(), lr=1e-4)
+        self.actor2_optimizer = torch.optim.Adam(self.actor2.parameters(), lr=1e-4)
+        self.critic2_optimizer = torch.optim.Adam(self.critic2.parameters(), lr=1e-4)
+        self.local_net_optimizer = torch.optim.Adam(self.local_net.parameters(), lr=1e-4)
+
+        self.logger = {
+                'actor1_losses': [],
+                'critic1_losses': [],
+                'actor2_losses': [],
+                'critic2_losses': [],
+                'local_net_losses': [],
+        }
+
+
+    def train(self, video, org_video):
+        obs_1, obs_2, acs_1, ac_2, log_prob_1, log_prob_2, rtg = self.rollout(video, org_video)
+        self.ppo(2, (obs_2, ac_2, log_prob_2, rtg))
+        self.ppo(1, (obs_1, acs_1, log_prob_1, rtg))
+
+
+    def forward(self, video):
+        #TODO: run inference
+        pass
+    
+    def rollout(self, video, org_video):
+        curr_perceptual_loss = [self.lpips(y_star, y) for y_star, y in zip(video, org_video)]
+        obs_1 = []
+        obs_2 = []
+        acs_1 = []
+        ac_2 = []
+        log_prob_1 = []
+        log_prob_2 = []
+        rewards = []
+        for i in range(self.time_steps):
+            #step through every model and get to image, conditioning to feed into local_net
+            #append to obs_1, obs_2, acs_1, ac_2, log_prob_1, log_prob_2 using the two get_log_prob methods
+            #TODO
+
+
+            image = []
+            conditioning = []
+            y_hat, reward = self.train_local_network(image, conditioning, org_video["TARGET NUMBER"])
+            rewards.append(-(reward - curr_perceptual_loss["TARGET NUMBER"]))
+            curr_perceptual_loss[i] = reward
+        rtg = self.compute_rewards_to_go(rewards)
+        
+        return obs_1, obs_2, acs_1, ac_2, log_prob_1, log_prob_2, rtg
+
+
+    def train_local_network(self, images, conditioning, org_images):
+        y_hat = self.local_net(images, conditioning)
+        loss = self.lpips(y_hat, org_images, normalize=True)
+        self.local_net_optimizer.zero_grad()
+        loss.backward()
+        self.local_net_optimizer.step()
+        self.logger['local_net_losses'].append(loss.detach())
+        return y_hat, loss.detach()
+
+    def compute_rewards_to_go(self, rewards, gamma=1):
+        b, t = rewards.size()  # Get the batch size and sequence length
+        rewards_to_go = torch.zeros_like(rewards)  # Initialize a tensor of rewards-to-go with the same shape as rewards
+
+        for i in range(b):  # Iterate through each batch
+            reward_to_go = 0  # Initialize the reward-to-go for the last step as 0
+            for j in reversed(range(t)):  # Iterate through the rollout in reverse order (from the last step to the first step)
+                reward_to_go = rewards[i, j] + gamma * reward_to_go  # Calculate the reward-to-go for the current step
+                rewards_to_go[i, j] = reward_to_go  # Store the calculated reward-to-go in the rewards_to_go tensor
+
+        return rewards_to_go
+
+
+    def ppo(self, net_num, info, n_updates_per_iteration=5):                                                                
+        if net_num == 1:
+            actor = self.actor1
+            critic = self.critic1
+            actor_optim = self.actor1_optimizer
+            critic_optim = self.critic1_optimizer
+            actor_losses = self.logger['actor1_losses']
+            critic_losses = self.logger['critic1_losses']
+
+        elif net_num == 2:
+            actor = self.actor2
+            critic = self.critic2
+            actor_optim = self.actor2_optimizer
+            critic_optim = self.critic2_optimizer
+            actor_losses = self.logger['actor2_losses']
+            critic_losses = self.logger['critic2_losses']
+
+        obs, acs, log_prob, rtgs = info                
+        V = critic(obs)
+        A_k = rtgs - V.detach()                                                                  
+        A_k = (A_k - A_k.mean()) / (A_k.std() + 1e-10)
+
+
+        for _ in range(n_updates_per_iteration):                                                    
+            V = critic(obs)
+            curr_log_prob = actor.get_log_prob(obs, acs)
+            ratio = torch.exp(curr_log_prob - log_prob)
+
+            L1 = ratio * A_k
+            L2 = torch.clamp(ratio, 1 - self.clip, 1 + self.clip) * A_k
+
+            actor_loss = -torch.min(L1, L2).mean()
+            critic_loss = torch.nn.MSELoss()(V, rtgs)
+
+            actor_optim.zero_grad()
+            actor_loss.backward()
+            actor_optim.step()
+
+            critic_optim.zero_grad()
+            critic_loss.backward()
+            critic_optim.step()
+
+            actor_losses.append(actor_loss.detach())
+            critic_losses.append(critic_loss.detach())
+
