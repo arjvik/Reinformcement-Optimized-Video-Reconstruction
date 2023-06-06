f7a046762578062eb87ca255714938b74ae66030
Aaditya-Prasad <50385321+Aaditya-Prasad@users.noreply.github.com>
2023-06-06 00:22:09 -0700

    bigger batch size

--- rovr/train_local_net.py
+++ rovr/train_local_net.py
@@ -28,38 +28,46 @@ class ImageDataset(Dataset):
     
     def __getitem__(self, idx):
         #pick two distinct numbers between 5 and 10
-        n = random.randint(5, 10)
-        m = random.randint(5, 10)
+        n = random.randint(5, 10) * (2 * random.randint(0, 1) - 1)
+        m = n
         while n == m:
-            m = random.randint(5, 10)
-        return self.video[idx], self.video(idx + n % self.num_frames), self.video(idx + m % self.num_frames), self.org_video[idx]
+            m = random.randint(5, 10) * (2 * random.randint(0, 1) - 1)
+        l = idx//25
+        n = l + (idx+n)%25
+        m = l + (idx+m)%25
+        return self.video[idx], self.video(n), self.video(m), self.org_video[idx]
     
-def load_video_dataset(root_folder, batch_size=1, num_workers=0, transform=None):
-    dataset = VideoDataset(root_folder, transform=transform)
+def load_video_dataset(root_folder, batch_size=1, num_workers=0):
+    dataset = VideoDataset(root_folder)
     return DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, shuffle = True)
 
-def load_image_dataset(video, org_video, batch_size=32, num_workers=0, transform=None):
-    dataset = ImageDataset(video, org_video, transform=transform)
+def load_image_dataset(video, org_video, batch_size=32, num_workers=0):
+    dataset = ImageDataset(video, org_video)
     return DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, shuffle = True)
 
 local_net = LocalNetwork()
 local_net.cuda()
 local_net_optimizer = torch.optim.Adam(local_net.parameters(), lr=1e-4)
+big_video = []
+big_org_video = []
 ds = load_video_dataset("out/LQ", batch_size = 1, num_workers = 32)
 for i, batch in enumerate(ds):
     video, org_video, _ = batch
     video = video.cuda()
     org_video = org_video.cuda()
-    image_ds = load_image_dataset(video, org_video, batch_size = 4, num_workers = 32)
-    for j, image_batch in enumerate(image_ds):
-        target, c1, c2, org = image_batch
-        local_net_optimizer.zero_grad()
-        image = torch.stack([target, c1, c2], dim = 1).cuda()
-        y_hat = local_net(image)
-        loss = F.mse_loss(y_hat, org)
-        loss.backward()
-        local_net_optimizer.step()
-        print(loss.item())
+    # image_ds = load_image_dataset(video, org_video, batch_size = 4, num_workers = 32)
+    big_video.append(video)
+    big_org_video.append(org_video)
+image_ds = load_image_dataset(torch.cat(big_video, dim = 0), torch.cat(big_org_video, dim = 0), batch_size = 32, num_workers = 32)
+for j, image_batch in enumerate(image_ds):
+    target, c1, c2, org = image_batch
+    local_net_optimizer.zero_grad()
+    image = torch.stack([target, c1, c2], dim = 1).cuda()
+    y_hat = local_net(image)
+    loss = F.mse_loss(y_hat, org)
+    loss.backward()
+    local_net_optimizer.step()
+    print(loss.item())
 
 
 
