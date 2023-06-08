90cc7363b5bf783ddb4ffb4369b0f04116c81d33
Aaditya-Prasad <50385321+Aaditya-Prasad@users.noreply.github.com>
2023-06-08 15:34:37 -0700

    updated explicit masking

--- rovr/video_ds_explicit.py
+++ rovr/video_ds_explicit.py
@@ -14,6 +14,24 @@ class VideoDataset2(Dataset):
         self.brightness = 40
         self.noise = 20
         self.difficulty = difficulty
+        self.l = np.random.permutation(20)[:7]
+        self.f = np.random.permutation(20)
+        self.helper = [[i, i] for i in range(6)]
+
+    def new_random(self):
+        self.l = np.random.permutation(20)[:7]
+        self.f = np.random.permutation(20)
+        f = self.f
+        self.helper = [
+            [f[0], f[1], f[4], f[5]], 
+            [f[2], f[3], f[6], f[7]],
+            [f[8], f[9], f[12], f[13]],
+            [f[10], f[11], f[14], f[15]],
+            [f[16], f[17]],
+            [f[18], f[19]]
+        ]
+    def __len__(self):
+        return len(self.subfolders) * 2 # each folder corresponds to two videos
 
     def corrupt_mask(self, frame, frame_index, mask):
         h, w, _ = frame.shape
@@ -32,10 +50,10 @@ class VideoDataset2(Dataset):
         raster_center_x += jitter_x
         raster_center_y += jitter_y
 
-        start_x = max(0, raster_center_x - (225 // 2) // 2)
-        end_x = min(w, start_x + 225 // 2)
-        start_y = max(0, raster_center_y - (125 // 2) // 2)
-        end_y = min(h, start_y + 125 // 2)
+        start_x = max(0, raster_center_x - (200 // 2) // 2)
+        end_x = min(w, start_x + 200 // 2)
+        start_y = max(0, raster_center_y - (100 // 2) // 2)
+        end_y = min(h, start_y + 100 // 2)
 
         mask[start_y:end_y, start_x:end_x, :] = 0
 
@@ -52,26 +70,24 @@ class VideoDataset2(Dataset):
 
         return corrupted_frame, mask
 
-        
-
-
-    def __len__(self):
-        return len(self.subfolders) * 2 # each folder corresponds to two videos
 
     def __getitem__(self, idx):
         subfolder = self.subfolders[idx // 2]
         video_folder = os.path.join(self.root_folder, subfolder)
         frames = sorted(os.listdir(video_folder))
 
+        self.new_random()
+        frame_masks = self.choose_frame_masks()
+        solutions = self.generate_solutions()
+
         left_video = []
         right_video = []
+        
         if (len(frames) != 50):
             print(f"ERROR {len(frames)=} {video_folder=}")
-        indices = np.random.permutation(25)
-        locations = np.random.permutation(25)[:7]
-        locations = self.location_helper(locations)
-        for i in range(0, 50, 2):
-            frame_path = os.path.join(video_folder, frames[indices[i//2]])
+
+        for i in range(0, 40, 2):
+            frame_path = os.path.join(video_folder, frames[self.f[i//2]])
             frame = cv2.imread(frame_path)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             frame = cv2.resize(frame, (1024, 512)) # resize the frame to 1024x512
@@ -81,10 +97,10 @@ class VideoDataset2(Dataset):
             right_frame = cv2.resize(right_frame, (256, 256)) # resize the right frame to 256x256
 
             if idx % 2 == 0:
-                corrupted, mask = self.corrupt_frame(left_frame, locations[i//2])
+                corrupted, mask = self.corrupt_frame(left_frame, frame_masks[i//2])
                 left_video.append((corrupted, left_frame, mask))
             else:
-                corrupted, mask = self.corrupt_frame(right_frame, locations[i//2])
+                corrupted, mask = self.corrupt_frame(right_frame, frame_masks[i//2])
                 right_video.append((corrupted, right_frame, mask))
 
         if idx % 2 == 0:
@@ -92,27 +108,64 @@ class VideoDataset2(Dataset):
         else:
             corrupted_frames, frames, masks = zip(*right_video)
 
-        return torch.from_numpy(np.array(corrupted_frames)).permute(0, 3, 1, 2) / 255, torch.from_numpy(np.array(frames)).permute(0, 3, 1, 2) / 255, torch.from_numpy(np.array(masks)).permute(0, 3, 1, 2) / 255
+        return torch.from_numpy(np.array(corrupted_frames)).permute(0, 3, 1, 2) / 255, torch.from_numpy(np.array(frames)).permute(0, 3, 1, 2) / 255, torch.from_numpy(np.array(masks)).permute(0, 3, 1, 2) / 255, torch.from_numpy(solutions)
 
-    def location_helper(self, locations):
-        frames = []
-        for i in range(25):
-            if i in [0, 1, 5, 6]:
-                frames.append([locations[0], locations[1], locations[2], locations[6]])
-            elif i in [2, 3, 7, 8]:
-                frames.append([locations[0], locations[1], locations[3], locations[5]])
-            elif i in [4, 9]:
-                frames.append([locations[0], locations[1], locations[2], locations[3]])
-            elif i in [10, 11, 15, 16]:
-                frames.append([locations[0], locations[2], locations[4], locations[5]])
-            elif i in [12, 13, 17, 18]:
-                frames.append([locations[1], locations[3], locations[4], locations[6]])
-            elif i in [14, 19]:
-                frames.append([locations[1], locations[2], locations[3], locations[4]])
-            elif i in [20, 21]:
-                frames.append([locations[0], locations[2], locations[4], locations[6]])
-            elif i in [22, 23]:
-                frames.append([locations[0], locations[3], locations[4], locations[5]])
-            elif i in [24]:
-                frames.append([locations[0], locations[2], locations[3], locations[4]])
-        return frames
+    def choose_frame_masks(self):
+        frame_masks = []
+        for i in range(20):
+            if i in self.helper[0]:
+                frame_masks.append([self.l[0], self.l[1], self.l[3], self.l[5]])
+            elif i in self.helper[1]:
+                frame_masks.append([self.l[0], self.l[1], self.l[4], self.l[6]])
+            elif i in self.helper[2]:
+                frame_masks.append([self.l[1], self.l[2], self.l[3], self.l[6]])
+            elif i in self.helper[3]:
+                frame_masks.append([self.l[1], self.l[2], self.l[4], self.l[5]])
+            elif i in self.helper[4]:
+                frame_masks.append([self.l[0], self.l[2], self.l[3], self.l[5]])
+            elif i in self.helper[5]:
+                frame_masks.append([self.l[0], self.l[2], self.l[4], self.l[6]])
+        return np.array(frame_masks)
+    
+    def generate_solutions(self):
+        solutions = np.empty((20, 32, 2))
+        for i in range(20):
+            if i in self.helper[0]:
+                solutions[i] = np.concatenate((
+                    np.array([[p, q] for p in self.helper[2] for q in self.helper[5]]),
+                    np.array([[p, q] for p in self.helper[3] for q in self.helper[4]]), 
+                    np.array([[q, p] for p in self.helper[2] for q in self.helper[5]]),
+                    np.array([[q, p] for p in self.helper[3] for q in self.helper[4]])
+                    ), axis = 0)
+            elif i in self.helper[1]:
+                solutions[i] = np.concatenate((
+                    np.array([[p, q] for p in self.helper[2] for q in self.helper[4]]),
+                    np.array([[p, q] for p in self.helper[3] for q in self.helper[4]]), 
+                    np.array([[q, p] for p in self.helper[2] for q in self.helper[4]]),
+                    np.array([[q, p] for p in self.helper[3] for q in self.helper[4]])
+                    ), axis = 0)
+            elif i in self.helper[2]:
+                solutions[i] = np.concatenate((
+                    np.array([[p, q] for p in self.helper[0] for q in self.helper[5]]),
+                    np.array([[p, q] for p in self.helper[1] for q in self.helper[5]]), 
+                    np.array([[q, p] for p in self.helper[0] for q in self.helper[5]]),
+                    np.array([[q, p] for p in self.helper[1] for q in self.helper[5]])
+                    ), axis = 0)
+            elif i in self.helper[3]:
+                solutions[i] = np.concatenate((
+                    np.array([[p, q] for p in self.helper[0] for q in self.helper[5]]),
+                    np.array([[p, q] for p in self.helper[1] for q in self.helper[4]]), 
+                    np.array([[q, p] for p in self.helper[0] for q in self.helper[5]]),
+                    np.array([[q, p] for p in self.helper[1] for q in self.helper[4]])
+                    ), axis = 0)
+            elif i in self.helper[4]:
+                solutions[i] = np.concatenate((
+                    np.array([[p, q] for p in self.helper[1] for q in self.helper[2]]),
+                    np.array([[q, p] for p in self.helper[1] for q in self.helper[2]]), 
+                    ), axis = 0)
+            elif i in self.helper[5]:
+                solutions[i] = np.concatenate((
+                    np.array([[p, q] for p in self.helper[0] for q in self.helper[2]]),
+                    np.array([[q, p] for p in self.helper[0] for q in self.helper[2]]), 
+                    ), axis = 0)
+        return solutions
